3663d27adc4b6af685d81839ac4929eb14e01aa2
AfzalIsmail <47380696+AfzalIsmail@users.noreply.github.com>
2019-02-21 15:35:32 +0400

    Add files via upload

--- src/alertBox.java
+++ src/alertBox.java
@@ -14,19 +14,24 @@ public class alertBox {
         window.initModality(Modality.APPLICATION_MODAL);
 
         window.setTitle(title);
-        window.setMinWidth(250);
+        window.setMinWidth(300);
+        window.setMinHeight(200);
 
         Label label = new Label();
 
         label.setText(message);
-        Button closeButton = new Button("Close window");
+        
+        Button closeButton = new Button("Ok");
 
         closeButton.setOnAction(e -> window.close());
+
 
         VBox layout = new VBox(10);
         layout.getChildren().addAll(label, closeButton);
         //set alignment to center of window
         layout.setAlignment(Pos.CENTER);
+
+       // closeButton.setBorder(new Insets(30,10,10,10));
 
         Scene scene = new Scene(layout);
         window.setScene(scene);
---
+++ src/logFile.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class logFile {
+
+public static ArrayList<String> logMessages = new ArrayList<>();
+
+    public static void addToLog(String logMessage){
+
+
+        logMessages.add(logMessage);
+
+
+    }
+
+    public static void printLog(){
+
+        for(String log: logMessages){
+            System.out.println(log);
+        }
+    }
+
+
+}
